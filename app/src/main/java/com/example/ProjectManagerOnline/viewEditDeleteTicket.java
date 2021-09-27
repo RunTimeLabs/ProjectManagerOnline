769352cfb380a53d425d7d9bfe98b79fa22c6f49
ob-hettiarachchi <osanda.buwaneka@gmail.com>
2021-09-27 16:50:22 +0530

    view/edit/delete

---
+++ app/src/main/java/com/example/ProjectManagerOnline/viewEditDeleteTicket.java
@@ -0,0 +1,14 @@
+package com.example.ProjectManagerOnline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewEditDeleteTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_edit_delete_ticket);
+    }
+}
