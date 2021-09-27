e0b880b565dbed055fe05362891f9883e26112c8
ob-hettiarachchi <osanda.buwaneka@gmail.com>
2021-09-27 21:15:55 +0530

    Ticket View all
    no errors

---
+++ app/src/main/java/com/example/ProjectManagerOnline/ticketAdapter.java
@@ -0,0 +1,53 @@
+package com.example.ProjectManagerOnline;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class ticketAdapter extends FirebaseRecyclerAdapter <Category, ticketAdapter.ticketViewHolder>{
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public ticketAdapter(@NonNull FirebaseRecyclerOptions<Category> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ticketViewHolder holder, int position, @NonNull Category model) {
+
+        holder.ticketCat.setText(model.getTicketCategory());
+        holder.ticketPrice.setText(model.getTicketPrice());
+
+    }
+
+    @NonNull
+    @Override
+    public ticketViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ticket_cat_item, parent, false);
+
+        return new ticketViewHolder(view);
+    }
+
+    class ticketViewHolder extends RecyclerView.ViewHolder{
+
+        TextView ticketCat,ticketPrice;
+
+        public ticketViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ticketCat = (TextView) itemView.findViewById(R.id.cattext);
+            ticketPrice = (TextView) itemView.findViewById(R.id.cattext);
+        }
+    }
+}
--- app/src/main/java/com/example/ProjectManagerOnline/viewEditDeleteTicket.java
+++ app/src/main/java/com/example/ProjectManagerOnline/viewEditDeleteTicket.java
@@ -1,14 +1,45 @@
 package com.example.ProjectManagerOnline;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class viewEditDeleteTicket extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ticketAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_edit_delete_ticket);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<Category> options =
+                new FirebaseRecyclerOptions.Builder<Category>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Category"), Category.class)
+                        .build();
+
+        mainAdapter = new ticketAdapter(options);
+        recyclerView.setAdapter(mainAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mainAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapter.stopListening();
     }
 }
