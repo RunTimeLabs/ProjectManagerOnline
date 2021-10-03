05490136c276efddf0e3da51a8b172128b193d59
ob-hettiarachchi <osanda.buwaneka@gmail.com>
2021-10-04 02:48:26 +0530

    sum

---
+++ MadProject - RunTime Labs/app/src/main/java/com/sliit/madproject/ticketCal.java
@@ -0,0 +1,15 @@
+package com.sliit.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class ticketCal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_cal);
+    }
+}
